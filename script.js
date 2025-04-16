8ce7c9d550537a5d9e7e56500f405952ed50e03d
KWalkowiak <krzys2007.pl@gmail.com>
2025-04-16 22:55:17 +0200

    16.04.2025 - dom

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const submitBtn = document.getElementById("submit");
 const resetBtn = document.getElementById("reset");
-const display = document.querySelector(".display-form");
+const displayTab = document.getElementById("display-table");
+const tabtainer = document.querySelector(".tabtainer");
+
+function updateTabtainerVisibility() {
+  if (displayTab.getElementsByTagName("tr").length > 0) {
+    tabtainer.style.display = "flex";
+  }
+}
+
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
   let nameValue = document.getElementById("imie").value;
   let mailValue = document.getElementById("e-mail").value;
-  const msg = document.createElement("p");
-  msg.innerText = `${nameValue} ${mailValue}`;
-  display.append(msg);
+  let textValue = document.getElementById("text").value;
+  const msg = document.createElement("tr");
+  msg.innerHTML = `<td>${nameValue}</td><td>${mailValue}</td><td class="text-td">${textValue}</td>`;
+  displayTab.append(msg);
+  updateTabtainerVisibility();
 });
