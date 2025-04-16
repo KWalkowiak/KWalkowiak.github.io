9caaaf8eaab96f6683bd98ae9fc7eda389779c05
KWalkowiak <krzys2007.pl@gmail.com>
2025-04-16 14:51:07 +0200

    16.04.2025 - szkoła

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const submitBtn = document.getElementById("submit");
 const resetBtn = document.getElementById("reset");
 const display = document.querySelector(".display-form");
-submitBtn.addEventListener("click", function () {
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
   let nameValue = document.getElementById("imie").value;
   let mailValue = document.getElementById("e-mail").value;
   const msg = document.createElement("p");
-  msg.innerText = `${nameValue}\n${mailValue}`;
+  msg.innerText = `${nameValue} ${mailValue}`;
   display.append(msg);
 });
